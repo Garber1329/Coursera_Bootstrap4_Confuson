4b20062ec954d3f5c5331fb257c61dc9bdff4410
Vitalii Chaikovskyi <garber1329@gmail.com>
2022-05-12 13:01:24 +0300

    Gulp Part 1 (Not working)

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var gulp = require('gulp'),
+   sass = require('gulp-sass'),
+   browserSync = require('browser-sync');
+   //  del = require('del'),
+   //  imagemin = require('gulp-imagemin'),
+   //  uglify = require('gulp-uglify'),
+   //  usemin = require('gulp-usemin'),
+   //  rev = require('gulp-rev'),
+   //  cleanCss = require('gulp-clean-css'),
+   //  flatmap = require('gulp-flatmap'),
+   //  htmlmin = require('gulp-htmlmin');
+
+
+gulp.task('sass', function () {
+    return gulp.src('./css/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('.//css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./css/*.scss', ['sass']);
+});
+
+gulp.task('browser-sync', function () {
+    var files = [
+        './*.html',
+        './css/*.css',
+        './img/*.{png,jpg,gif}',
+        './js/*.js'
+    ];
+
+    browserSync.init(files, {
+        server: {
+            baseDir: "./"
+        }
+    });
+
+});
+
+// Default task
+gulp.task('default', gulp.series('browser-sync', function () {
+    gulp.start('sass:watch');
+}));
